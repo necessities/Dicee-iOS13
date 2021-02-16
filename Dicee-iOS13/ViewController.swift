c64c44139b30d146efeebf2ae6fa3fad10af70a4
“necessities” <bear_necessities@icloud.com>
2021-02-15 20:17:52 -0500

    update to code for button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,20 +14,43 @@ class ViewController: UIViewController {
     // IBOutlet allows referenecing to UI elements
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
+        //diceImageView1.alpha = 0.5
         //diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
         //who.what = value
+        
+        
+        
+        
+        
     }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped.")
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        //print("Button got tapped.")
+        //diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        //diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        
+        
+        
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
+        
+        rightDiceNumber = rightDiceNumber - 1
+        leftDiceNumber = leftDiceNumber + 1
+        
+        
     }
     
 }
 
+ 
